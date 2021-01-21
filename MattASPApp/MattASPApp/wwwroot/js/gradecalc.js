87235c5f75a7ce1950cee539a77bf3a8374a9a7e
MattEBoy <teo1997@gmail.com>
2021-01-20 18:56:45 -0700

    Add files via upload

---
+++ MattASPApp/MattASPApp/wwwroot/js/gradecalc.js
@@ -0,0 +1,19 @@
+﻿var submit = document.getElementById("submit")
+
+submit.addEventListener("click", function () {
+    var gradeCalc = 0;
+
+
+    var asg = document.getElementById("Assignments").value
+    var proj = document.getElementById("Projects").value
+    var quiz = document.getElementById("Quizzes").value
+    var exam = document.getElementById("Exams").value
+    var intex = document.getElementById("INTEX").value
+    //Projects
+    gradeCalc = parseInt(asg)*0.5 + parseInt(proj)*0.1 + parseInt(quiz)*0.1 + parseInt(exam)*0.2 + parseInt(intex)*0.1;
+
+
+    document.getElementById("expected_grade").innerHTML = "Expected Grade: " + gradeCalc + "%";
+    alert(gradeCalc)
+
+});
